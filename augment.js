0faa9b9f6302698a371872941ab041fa6529cb86
Christina <Christina@Billys-MacBook-Pro.local>
2016-07-07 22:03:04 -0500

    first commit

---
+++ augment.js
@@ -0,0 +1,12 @@
+var CarLot = (function () {
+
+buildCar: function (carCard) {
+  console.log('hello', inventory);
+  var newCar;
+  inventory.forEach(function (value, i) {
+    console.log('value', value);
+    console.log('i', i);
+  })
+}
+
+})();
